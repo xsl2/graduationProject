f69498bdb55305980ef76e1fd4b5bddacdc0a8e6
xushilong <2368823069@qq.com>
2025-01-12 23:51:01 +0800

    aaa

--- app/src/main/java/com/example/schoolhelper/entity/Student.kt
+++ app/src/main/java/com/example/schoolhelper/entity/Student.kt
@@ -1,21 +1,11 @@
 package com.example.schoolhelper.entity
 
-import kotlin.math.max
-
 class Student {
-    var account=""
-    var password=""
+    private var account=""
+    private var password=""
     override
     fun toString(): String {
         return "Student(account='$account', password='$password')"
     }
 
-}
-fun max1(num1:Int,num2:Int):Int= max(num1,num2)
-fun main()
-{
-    var a=10
-    var b=20
-    var ans= max1(a,b)
-    println("hello kotlin"+ans)
 }

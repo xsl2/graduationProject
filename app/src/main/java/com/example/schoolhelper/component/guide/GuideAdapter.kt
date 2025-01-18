a753b26de97d36a961aecd6ad0684684480c8ceb
xushilong <2368823069@qq.com>
2025-01-18 23:24:31 +0800

    7 引导界面完成后对GuideAdapter进行重构

--- app/src/main/java/com/example/schoolhelper/component/guide/GuideAdapter.kt
+++ app/src/main/java/com/example/schoolhelper/component/guide/GuideAdapter.kt
@@ -3,24 +3,10 @@ package com.example.schoolhelper.component.guide
 import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStatePagerAdapter
+import com.example.schoolhelper.adapter.BaseFragmentPagerAdapter
 
-class GuideAdapter(val context:Context,fragmentManager:FragmentManager): FragmentStatePagerAdapter(fragmentManager) {
-    private var datum:MutableList<Int> = mutableListOf()
-    override fun getCount(): Int {
-
-        return datum.size
-    }
-
+class GuideAdapter(context:Context,fragmentManager:FragmentManager): BaseFragmentPagerAdapter<Int>(context,fragmentManager) {
     override fun getItem(position: Int): Fragment {
-        return GuideFragment.newInstance(datum[position])
-
+        return GuideFragment.newInstance(getData(position))
     }
-
-    fun setDatum(datum: MutableList<Int>) {
-        this.datum.clear()
-        this.datum.addAll(datum)
-        notifyDataSetChanged()
-    }
-
 }
